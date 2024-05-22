48af4390d3aa42211c67bf2aec8f7f08f2d15615
Erik <erik.pustoslemsek@gmail.com>
2024-05-22 14:48:44 +0200

    Statistics layout

---
+++ src/components/Statistics_layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Statistics_layout = () => {
+    return (
+        <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 mt-4 flex'>
+            <div className='bg-white rounded-md p-3 w-1/2 flex-grow'>
+                <h2>Main Content</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Statistics_layout;
--- src/pages/personal/statistics.tsx
+++ src/pages/personal/statistics.tsx
@@ -1,8 +1,15 @@
+import Navbar from '@/components/Navbar'
+import Statistics_layout from '@/components/Statistics_layout'
 import React from 'react'
 
 const Statistics = () => {
     return (
-        <div>statistics</div>
+        <>
+        <div className='bg-gray-100 min-h-screen'>
+            <Navbar />
+            <Statistics_layout />
+        </div>
+        </>
     )
 }
 
