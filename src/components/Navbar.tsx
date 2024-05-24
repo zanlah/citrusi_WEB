985455973ae0df128221ea877fecfafe8bc9d880
Erik <erik.pustoslemsek@gmail.com>
2024-05-24 14:34:14 +0200

    Added menu item for personal statistics

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -94,6 +94,16 @@ export default function Navbar() {
                                             <Menu.Item>
                                                 {({ active }) => (
                                                     <a
+                                                        href="/statistics"
+                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                    >
+                                                        Personal stats
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
                                                         href="#"
                                                         className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                                     >
